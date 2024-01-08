4d6fad9b2edc0fe468f02f4859ec00c99f279fc0
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-01-08 15:07:27 +1100

    💖 tidy formatting

--- tutorial.js
+++ tutorial.js
@@ -1,15 +1,15 @@
 // Assuming you're familiar with Python, here's some code that
 // is the equivalent of what you're used to typing in Python.
 
-console.log('Hello, World!') // print('Hello, World!')
-alert('Hello, World!') // puts a popup on the screen
+console.log('Hello, World!'); // print('Hello, World!')
+alert('Hello, World!'); // puts a popup on the screen
 
 // In JavaScript, variables are defined with a 'let' or 'const'
 // keyword. We use 'let' when we want to allow the value of
 // the variable to be change, and we use 'const' when it's
 // never gonna change.
-let random_number = 5
-const pi = 3.14
+let random_number = 5;
+const pi = 3.14;
 
 // In JavaScript, you can end your lines of code with semicolons.
 // This isn't something you're probably used to in Python.
@@ -19,41 +19,41 @@ let z = 'hello';
 
 // If statements
 if (5 > 3) {
-    console.log('5 is greater than 3')
+  console.log('5 is greater than 3');
 } else if (10 < 15) {
-    console.log('10 is less than 15')
+  console.log('10 is less than 15');
 } else {
-    console.log('None of the above statements were true.')
+  console.log('None of the above statements were true.');
 }
 
 // When we check if something's equal, we usually use ===
 // instead of ==, whereas in Python you just use ==.
 if (5 === 5) {
-    console.log('5 is equal to 5')
+  console.log('5 is equal to 5');
 }
 
 // While loops are basically the same as Python.
 while (true) {
-    // ...
+  // ...
 }
 
-let x = 5
+let x = 5;
 while (x === 5) {
-    x--
+  x--;
 }
 
 // For loops are quite verbose in JS (and many other languages).
 
 for (let i = 0; i < 10; i++) {
-    // ...
+  // ...
 }
 
 // We can iterate over an array.
 
-let arr = [1, 2, 3]
+let arr = [1, 2, 3];
 arr.forEach((item) => {
-    // do stuff
-})
+  // do stuff
+});
 
 // There's a couple different ways to define functions.
 
@@ -63,18 +63,18 @@ arr.forEach((item) => {
 */
 
 function multiply(x, y) {
-    return x * y
+  return x * y;
 }
 
 // Or you can do it in one line this clever way...
 
-const multiply = (x, y) => x * y
+const multiply = (x, y) => x * y;
 
 // JavaScript supports dictionaries, like Python.
 // We typically them objects in JS, or more generally hashmaps/hash tables.
 const favourite_foods = {
-    'lachlan': 'ravioli',
-    'allyssa': 'biryani',
-    'sophie': 'pizza',
-    'katie': 'sushi'
-}
+  lachlan: 'ravioli',
+  allyssa: 'biryani',
+  sophie: 'pizza',
+  katie: 'sushi',
+};
